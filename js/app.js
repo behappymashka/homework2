2afdbde6c7d45c159804e06ee871a83c550f5c62
behappymashka <98Mashenka@gmail.com>
2024-02-04 10:17:55 +0200

    delete idea

---
+++ js/app.js
@@ -0,0 +1,23 @@
+//----------------------------------------1
+
+let string1 = prompt("Введите предложение 1 ");
+let string2 = prompt("Введите предложение 2 ");
+let string3 = prompt("Введите предложение 3 ");
+
+console.log(string1 + "\n" + string2 + "\n" + string3 &&
+    string2 + "\n" + string1 + "\n" + string3 && string3 + "\n" + string1 + "\n" + string2);
+
+
+//----------------------------------------2
+/*
+
+let number = prompt("Введите пятизначное число");
+
+let numeric = parseInt(number / 10000);
+let numeric2 = parseInt(number / 1000) % 10;
+let numeric3 = parseInt(number / 100) % 10;
+let numeric4 = parseInt(number / 10) % 10;
+let numeric5 = number % 10;
+
+console.log(numeric, numeric2, numeric3, numeric4, numeric5);
+*/
